ba6a81a600579304aea817f58b23d8fa4a59017f
AntonellaAB <antonellaabigahil@hotmail.com>
2026-03-07 20:14:51 -0300

    Second commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#Checkers - Level Medium 
+import pygame
+from checkers.constants import WIDTH, HEIGHT
+
+
+#constant values
+FPS = 60
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    
+
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get(): #check if there is any event going on 
+            if event.type == pygame.QUIT:
+                run == False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+    pygame.quit()
+
+
+
+
+
+
+main()
+
