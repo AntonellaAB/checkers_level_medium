56a75ade9b0ca6483e449ec49687d736faccb061
AntonellaAB <antonellaabigahil@hotmail.com>
2026-03-08 17:01:37 -0300

    Base_estructure

--- checkers/constants.py
+++ checkers/constants.py
@@ -1,6 +1,6 @@
 #All constant variables
 import pygame
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 700, 700
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH//COLS
 
@@ -9,4 +9,11 @@ RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
+GRAY = (128, 128, 128)
 
+#crown size
+#CROWN = pygame.image.load("assets/crown.png")
+#CROWN = pygame.transform.smoothscale(CROWN, (45, 25))
+CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'), (35, 25))
+
+#Nos quedamos en 7:17:00
